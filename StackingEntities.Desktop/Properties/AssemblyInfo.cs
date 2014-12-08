23783d21794f16faecf54298e85b1ef26fca5669
Sidneys1 <sidneys1@live.com>
2014-12-08 12:30:58 -0500

    Renamed Project Folders

--- StackingEntities.Desktop/Properties/AssemblyInfo.cs
+++ StackingEntities.Desktop/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StackingEntities")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Minecraft Summon Generator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Borne Programming")]
 [assembly: AssemblyProduct("StackingEntities")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
