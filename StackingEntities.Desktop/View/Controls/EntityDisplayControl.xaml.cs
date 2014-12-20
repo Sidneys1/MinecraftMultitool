6944f85424dd7f5a7c96d7ee92c35acc4e797c88
Sidneys1 <sidneys1@live.com>
2014-12-19 23:20:40 -0500

    Base EntityControl

---
+++ StackingEntities.Desktop/View/Controls/EntityDisplayControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace StackingEntities.Desktop.View.Controls
+{
+	/// <summary>
+	/// Interaction logic for EntityDisplayControl.xaml
+	/// </summary>
+	public partial class EntityDisplayControl
+	{
+		public EntityDisplayControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
